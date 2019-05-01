c5701f055a674e5d89df7ade05a7b86eb379d4aa
161240045-Lu Hao <593413198@qq.com>
2019-05-01 13:29:31 +0800

    update TicTacGame

--- Generator.cpp
+++ Generator.cpp
@@ -2,7 +2,6 @@
 int ans = 0; //所有局面数量
 
 int Go(int depth){
-	cout << "Now: " << depth << endl;
 	/* 表示进行到第depth回合
 	 * depth%2=0: turn=-1 
 	 * depth%2=1: turn=1  */
@@ -10,6 +9,7 @@ int Go(int depth){
 	else Turn = 1;
 	if (depth==9){
 		ans ++;
+		cout << "棋盘下满"<<endl;
 		return 1; //9回合到，棋盘落满，游戏结束
 	}
 	for (int i=0; i<3; i++){
@@ -17,11 +17,13 @@ int Go(int depth){
 			if (ChessBoard[i][j]==0){ //找到空位置就落子
 				ChessBoard[i][j] = Turn;
 			}
-			if (!CheckResult()) { //若未分出胜负，继续下一回合
+			int winner = CheckResult();
+			if (!winner) //若未分出胜负，继续下一回合
 				Go(depth+1);
+			else{//已分出胜负，不必继续搜索，回溯
+				cout << winner << "获胜" << endl;
+				ChessBoard[i][j] = 0;
 			}
-			//已分出胜负，不必继续搜索，回溯
-			ChessBoard[i][j] = 0;
 		}
 	}	
 }
